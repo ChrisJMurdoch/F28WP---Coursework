04ad8dee9ae62ae3528f626feded1ec5b82ce56b
Cameron-Douglas <55535960+Cameron-Douglas@users.noreply.github.com>
2019-10-03 14:56:24 +0100

    Additional queries for signing in and logging in added

--- Database/Database.sql
+++ Database/Database.sql
@@ -13,9 +13,12 @@ CREATE TABLE Users {
 
 /* QUERIES
 
+SIGN-UP Queries :
+
 INSERT INTO Users VALUES ("USERNAME","PASSWORD");
 INSERT INTO HighScore ("USERNAME", HIGHSCORE);
 
+ERROR HANDLING:
 
 BEGIN TRY
   INSERT INTO Users VALUES ("USERNAME","PASSWORD");
@@ -25,5 +28,13 @@ BEGIN CATCH
   PRINT ('this username already exists');
   THROW;
 END CATCH
+
+
+LOG-IN Queries:
+
+SELECT userName, password FROM Users WHERE "USERNAME" = Users.userName
+
+if password == "PASSWORD" -> allow login
+else -> password is incorrect please try again
 
 */
