83bc8cefebbc4a93d5ecc5d52a65db7e8a7e5e0a
JW148 <jw148@hw.ac.uk>
2019-10-03 10:53:57 +0100

    play button sends message to server + formatting

--- Client/Client.js
+++ Client/Client.js
@@ -1,34 +1,31 @@
-
-
-
 // SOCKET INTERFACE
 
 // Create WebSocket
-const socket = new WebSocket('ws://137.195.210.2:8001'); // --MSI Heriot-Watt
-//const socket = new WebSocket('ws://192.168.0.38:8001'); // --MSI Home
+//const socket = new WebSocket('ws://137.195.210.2:8001'); // --MSI Heriot-Watt
+const socket = new WebSocket('ws://192.168.0.38:8001'); // --MSI Home
 
 // EVENTS
 // Connection event
 socket.onopen = function () {
-  console.log('Server connection established.');
+    console.log('Server connection established.');
 };
 // Error event
 socket.onerror = function (error) {
-  console.log('Error: ', error);
+    console.log('Error: ', error);
 };
 // Ping event
 socket.onping = function (error) {
-  console.log('Error: ', error);
+    console.log('Error: ', error);
 };
 // Message event
 socket.onmessage = function (e) {
-  console.log('Received: ', e.data);
+    console.log('Received: ', e.data);
 };
 
 // METHODS
 function sendmessage(message) {
-  console.log('Sending: ', message);
-  socket.send(message);
+    console.log('Sending: ', message);
+    socket.send(message);
 }
 
 
@@ -37,27 +34,27 @@ function sendmessage(message) {
 
 // Set the enter key to activate send button
 var input = document.getElementById("sendmessage");
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-    document.getElementById("sendbutton").click();
-    input.value = '';
-  }
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        document.getElementById("sendButton").click();
+        input.value = '';
+    }
 });
 
 // On button press
 function sendField() {
-  var message = document.getElementById("sendmessage").value;
-  sendmessage(message);
+    var message = document.getElementById("sendmessage").value;
+    sendmessage(message);
 }
 
 function test() {
     document.getElementById("PlayMenu").style.visibility = 'hidden';
-    
+
     var element = document.getElementById("score");
     element.innerHTML = "Score: ";
-    
-    
-    
+
+
+
     var para = document.createElement("p");
     var node = document.createTextNode("Score: ");
     para.appendChild(node);
@@ -65,15 +62,20 @@ function test() {
     element.appendChild(para);
 }
 
-function btnPress(){
+function btnPress() {
     var s = document.getElementById("PlayMenu").style;
     s.opacity = 1;
-    (function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,40)})();
-    
-//    document.getElementById("PlayMenu").style.visibility = 'hidden';
-    
+    (function fade() {
+        (s.opacity -= .1) < 0 ? s.display = "none" : setTimeout(fade, 40)
+    })();
+
+    //    document.getElementById("PlayMenu").style.visibility = 'hidden';
+
     var table = "<table><tr><th>Username</th><th>Score</th></tr></table>";
+
+    var element = document.getElementById("Leaderboard").innerHTML = table;
     
-    var element = document.getElementById("Leaderboard").innerHTML = table ;
-    
+    var message = document.getElementById("UsernameInput").value;
+    sendmessage(message);
+
 }
