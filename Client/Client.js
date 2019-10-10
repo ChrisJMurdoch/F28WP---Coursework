66398e614cfdfc71dd413a7f82217f6890396df0
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2019-10-10 15:45:57 +0100

    Removed syntax errors.

--- Client/Client.js
+++ Client/Client.js
@@ -119,25 +119,25 @@ function submitBtnPress() {
 
     var usrName = document.getElementById("UsernameInput").value;
     var usrPsswd = document.getElementById("UsernamePassword").value;
-    
+
     if (usrName != "" && usrPsswd != "") {
         login(usrName, usrPsswd);
 
         var r = document.getElementById("UserLogin").style;
         r.opacity = 1;
-        
+
         changeBg("grey");
 
         (function fade() {
             (r.opacity -= .1) < 0 ? r.display = "none" : setTimeout(fade, 40)
         })();
-        
+
         var table = document.getElementById("Leaderboard").style.visibility = "visible";
         fade(table);
-        
+
         var length = document.getElementById("YourLength").style.visibility = "visible";
         fade(length);
-        
+
     } else{
         alert("You must enter username and password");
     }
@@ -146,10 +146,10 @@ function submitBtnPress() {
 function changeBg(color) {
     document.body.style.background = color;
 }
-<<<<<<< HEAD
-=======
+//<<<<<<< HEAD
+//=======
 
 function fade(element) {
     (element.opacity += .1) < 0 ? element.display = "none" : setTimeout(fade, 70)
         };
->>>>>>> 121389e2cc7602f07887cd7b81868edc6d9dda58
+//>>>>>>> 121389e2cc7602f07887cd7b81868edc6d9dda58
