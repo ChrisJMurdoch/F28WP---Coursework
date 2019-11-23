8c891773d6cf5eccbe4daee6e73e597511bf230f
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2019-11-23 13:16:19 +0000

    Cleaned up game code.

--- Server/scripts/game.js
+++ Server/scripts/game.js
@@ -1,33 +1,24 @@
+
+// VARIABLES
+
+// Use socket_handler to broadcast to clients
 var socket_handler = require('./socket_handler');
 
-// Intersect helper
-function hasIntersection( x1, y1, x2, y2, x3, y3, x4, y4 ) {
+// Milliseconds between each server tick
+const TICK_PERIOD = 5;
 
-  if (Math.abs(x1-x2) > 50 || Math.abs(x3-x4) > 50 || Math.abs(y1-y2) > 50 || Math.abs(y3-y4) > 50) {
-    return false;
-  }
+// List of players in game
+var players = [];
 
-  var firstvert = x1 === x2;
-  var secondvert = x3 === x4;
+// Queue to store requests from socket interrupts
+var queue = [];
 
-  if (firstvert === secondvert) { // Parralel
-    return false;
-  }
+// List to store last tick durations, for debugging.
+var load = [];
 
-  if (!firstvert) {
-    var xs = (x1<x3 && x3<x2) || (x2<x3 && x3<x1);
-    var ys = (y3<y1 && y1<y4) || (y4<y1 && y1<y3);
-    return ( xs ) && ( ys );
-  } else {
-    var xs = (x3<x1 && x1<x4) || (x4<x1 && x1<x3)
-    var ys = (y1<y3 && y3<y2) || (y2<y3 && y3<y1)
-    return ( xs ) && ( ys );
-  }
-}
-const TICK_PERIOD = 5; // in milliseconds
+// EXPORTS
 
-// INTERFACES
-// Check if player exists
+// Check if player exists in game
 exports.has_player = function(in_name) {
   for(var i in players) {
     if ( players[i].name === in_name ) {
@@ -54,113 +45,109 @@ exports.remove_player = function(in_player) {
   for(var i in players){
     if ( players[i].name === in_player.name ) {
      players.splice(i, 1);
+     break;
     }
   }
 };
 
 // Main loop
-var load = [];
 exports.start = function() {
+  console.log('STARTING GAME...')
+
+  // Start a repeated interval to process request queue
   setInterval(function run() {
+    // Get time for load logging
     var start_time = Date.now();
+    // Process each request in queue
     for (var i in queue) {
-      if (queue.length > 0) {
-        req = queue.shift();
-        var last = req.player.time;
-        req.player.time = Date.now();
-        var elapsed = req.player.time - last;
-        //req.player.x += req.x * elapsed / 10;
-        //req.player.y += req.y * elapsed / 10;
-        req.player.x.push(req.player.x[req.player.x.length-1] + req.x * elapsed / 10);
-        req.player.y.push(req.player.y[req.player.y.length-1] + req.y * elapsed / 10);
-        req.player.t.push(Date.now());
-        if (req.player.x[req.player.x.length-1] < 0) {
-          req.player.x[req.player.x.length-1] = 500;
-        } else if (req.player.x[req.player.x.length-1] > 500) {
-          req.player.x[req.player.x.length-1] = 0;
-        }
-        if (req.player.y[req.player.y.length-1] < 0) {
-          req.player.y[req.player.y.length-1] = 500;
-        } else if (req.player.y[req.player.y.length-1] > 500) {
-          req.player.y[req.player.y.length-1] = 0;
-        }
-        /*
-        if (req.player.x.length > 100) {
+      // Get first request
+      req = queue.shift();
+      // Get last time player was processed
+      var last = req.player.time;
+      // Set time player was processed
+      req.player.time = Date.now();
+      // Get time elapsed
+      var elapsed = req.player.time - last;
+      // Create new body part with time-adjusted coordinates
+      req.player.x.push(req.player.x[req.player.x.length-1] + req.x * elapsed / 10);
+      req.player.y.push(req.player.y[req.player.y.length-1] + req.y * elapsed / 10);
+      req.player.t.push(Date.now());
+      // Constrain new body part to 500 x 500 game map
+      if (req.player.x[req.player.x.length-1] < 0) {
+        req.player.x[req.player.x.length-1] = 500;
+      } else if (req.player.x[req.player.x.length-1] > 500) {
+        req.player.x[req.player.x.length-1] = 0;
+      }
+      if (req.player.y[req.player.y.length-1] < 0) {
+        req.player.y[req.player.y.length-1] = 500;
+      } else if (req.player.y[req.player.y.length-1] > 500) {
+        req.player.y[req.player.y.length-1] = 0;
+      }
+      // Cut old body parts
+      var time = Date.now();
+      while (true) {
+        if (time - req.player.t[0] > 2000) {
+          req.player.t.shift();
           req.player.x.shift();
           req.player.y.shift();
+        } else {
+          break;
         }
-        */
-        var time = Date.now();
-        while (true) {
-          if (time - req.player.t[0] > 2000) {
-            req.player.t.shift();
-            req.player.x.shift();
-            req.player.y.shift();
-          } else {
-            break;
-          }
-        }
-        //start
-        // Collision detection
-        if (req.player.x.length <=1) {
+      }
+      // Collision detection
+      // Escape if player has no body parts (lines)
+      if (req.player.x.length <= 1) {
+        continue;
+      }
+      // Check new body part against against each player
+      outer: for (var player in players) {
+        // Escape if other player has no body parts (lines)
+        if (players[player].x.length <=1) {
           continue;
         }
-        outer: for (var player in players) {
-          if (players[player].x.length <=1) {
-            continue;
-          }
-          //if (players[player].name === req.player.name) {
-          //  continue;
-          //}
-          for (var point in players[player].x) {
-            if (hasIntersection(
-              req.player.x[req.player.x.length-1],
-              req.player.y[req.player.y.length-1],
-              req.player.x[req.player.x.length-2],
-              req.player.y[req.player.y.length-2],
-              players[player].x[point],
-              players[player].y[point],
-              players[player].x[point-1],
-              players[player].y[point-1]
-            )) {
-              console.log('Collision!');
-              // Remove clientside
-              socket_handler.broadcast('6;' + req.player.name)
-              // Remove serverside
-              module.exports.remove_player(req.player);
-              var n = new Player(req.player.name);
-              players.push(n);
-              req.soc.player = n;
-              break outer;
-            }
+        // Check each body part in other player
+        for (var point in players[player].x) {
+          if (hasIntersection(
+            req.player.x[req.player.x.length-1],
+            req.player.y[req.player.y.length-1],
+            req.player.x[req.player.x.length-2],
+            req.player.y[req.player.y.length-2],
+            players[player].x[point],
+            players[player].y[point],
+            players[player].x[point-1],
+            players[player].y[point-1]
+          )) {
+            // Die
+            // Remove clientside
+            socket_handler.broadcast('6;' + req.player.name)
+            // Remove serverside
+            module.exports.remove_player(req.player);
+            var n = new Player(req.player.name);
+            players.push(n);
+            req.soc.player = n;
+            break outer;
           }
         }
-        // End collision detection
-        //end
-        pull_update(req.soc, req.player);
       }
+      // Update client
+      pull_update(req.soc, req.player);
     }
+    // Server load calculations
     var duration = Date.now() - start_time;
     load.push(duration);
+    // Trim load queue to 100 entries
     if (load.length > 100) {
       load.shift();
     }
   }, TICK_PERIOD);
-};
 
-setInterval(function dis() {
-  var av = 0;
-  for (var i in load) {
-    av += load[i];
-  }
-  av /= load.length;
-  // console.log('SERVER LOAD: ' + Math.floor(av / TICK_PERIOD * 100) + '%');
-}, 1000);
+  console.log('GAME STARTED.')
+};
 
 // PRIVATE METHODS
 
-// Get player data
-pull_update = function(in_socket, in_player) {
+// Send game data to client
+function pull_update(in_socket, in_player) {
   var response = '4';
   for (var i in players) {
     var s = ';' + players[i].name + '@' + Math.floor(players[i].x[players[i].x.length-1]) + '@' + Math.floor(players[i].y[players[i].y.length-1]);
@@ -169,9 +156,40 @@ pull_update = function(in_socket, in_player) {
   in_socket.send(response);
 };
 
-var players = [];
-var queue = [];
+// Intersect method
+function hasIntersection( x1, y1, x2, y2, x3, y3, x4, y4 ) {
+  // Ignore gaps; lag gaps and edge teleporting.  Consistant with client rendering
+  if (Math.abs(x1-x2) > 50 || Math.abs(x3-x4) > 50 || Math.abs(y1-y2) > 50 || Math.abs(y3-y4) > 50) {
+    return false;
+  }
 
+  // Get line orientations
+  var firstvert = x1 === x2;
+  var secondvert = x3 === x4;
+
+  // Parralel lines don't intersect
+  if (firstvert === secondvert) {
+    return false;
+  }
+
+  if (!firstvert) {
+    // Second line is vertical
+    // Check if linesoverlap on both planes
+    var xs = (x1<x3 && x3<x2) || (x2<x3 && x3<x1);
+    var ys = (y3<y1 && y1<y4) || (y4<y1 && y1<y3);
+    return ( xs ) && ( ys );
+  } else {
+    // First line is vertical
+    // Check if linesoverlap on both planes
+    var xs = (x3<x1 && x1<x4) || (x4<x1 && x1<x3)
+    var ys = (y1<y3 && y3<y2) || (y2<y3 && y3<y1)
+    return ( xs ) && ( ys );
+  }
+}
+
+// CLASSES
+
+// Request record
 class UpdateReq {
   constructor (in_player, in_x, in_y, in_socket) {
     this.player = in_player;
@@ -181,6 +199,7 @@ class UpdateReq {
   };
 };
 
+// CLient record
 class Player {
   constructor(in_name) {
     this.name = in_name;
