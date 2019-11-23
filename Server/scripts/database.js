25e849bc7e29885d982cbd3860ef973637187b93
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2019-11-23 12:32:03 +0000

    Cleaned up database code and logs.

--- Server/scripts/database.js
+++ Server/scripts/database.js
@@ -1,27 +1,29 @@
 
-// Modules
+// VARIABLES
+
+// Use mysql module for database connection
 const mysql = require("mysql");
 
-// Connection
+// Declare global connection object
 var db_connection;
+
+// EXPORTS
 
 // Connect to database
 exports.connect = function(DB_DATA, callback) {
   // Create connection
   db_connection = mysql.createConnection(DB_DATA);
   // Start connection
-  console.log("CONNECTING TO GAME DATABASE...");
+  console.log("CONNECTING TO DATABASE...");
   db_connection.connect(function(err) {
     if (err) throw err;
-    // setup error handling
+    // Setup error handling
     db_connection.on('error', function(err) {
-      console.log('ERR START - GDB');
-      console.log(err.code);
-      console.log('ERR END');
-      console.log('RESTARTING...');
+      // Restart connection
+      console.log('RESTARTING DATABASE CONNECTION...');
       module.exports.connect(DB_DATA, function() {})
     });
-    console.log("CONNECTION TO GAME DATABASE ESTABLISHED.\n");
+    console.log("CONNECTION TO GAME DATABASE ESTABLISHED.");
     callback();
   });
 };
@@ -29,6 +31,7 @@ exports.connect = function(DB_DATA, callback) {
 // Database validation
 exports.verify = function(username, password, callback) {
   user(username, function(result) {
+    // User exists and password matches
     callback(result != undefined && result.password === password);
   });
 };
@@ -36,9 +39,9 @@ exports.verify = function(username, password, callback) {
 // Display database users
 exports.print_users = function() {
   users(function(result) {
-    console.log('USER-PASSWORD PAIRS:');
+    console.log('USER - PASSWORDS:');
     for (var i in result) {
-      console.log(result[i].userName + ' ' + result[i].password);
+      console.log(' - ' + result[i].userName + ' - ' + result[i].password);
     }
     console.log();
   });
@@ -51,22 +54,26 @@ exports.add_user = function(username, password) {
   db_connection.query(sql, [user], function (error, results) {
     if (!error) {
       console.log('USER ADDED.');
+    } else {
+      console.log('USER ADD FAILED.');
     }
   });
 };
 
-// Check user
+// Check if username exists
 exports.check_user = function(username, callback) {
   user(username, function(user) {
     if(user) {
-      console.log('FOUND');
+      console.log('USER FOUND.');
       callback(true);
     } else {
-      console.log('NOTFOUND');
+      console.log('USER NOT FOUND.');
       callback(false);
     }
   });
 }
+
+// PRIVATE METHODS
 
 // Get user
 user = function (username, callback) {
