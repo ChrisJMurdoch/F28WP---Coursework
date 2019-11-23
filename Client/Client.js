8f480be4e3562df892643c611c3e669b3baa0663
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2019-11-23 21:17:40 +0000

    Cleaned up folders.

--- Client/Client.js
+++ Client/Client.js
@@ -7,10 +7,12 @@ const LOGIN = '2';
 const CLIENT_TO_SERVER_COORDS = '3';
 const SERVER_TO_CLIENT_COORDS = '4';
 const LOGIN_SUCCESS = '5';
+const DEATH = '6';
+const LEADERBOARD = '7';
 
 // Create WebSocket
-//const socket = new WebSocket('ws://137.195.109.210:8001'); // --MSI Heriot-Watt
-const socket = new WebSocket('ws://f28wp.herokuapp.com/:80'); // --MSI Home
+//const socket = new WebSocket('ws://localhost:80'); // --Localhost
+const socket = new WebSocket('ws://f28wp.herokuapp.com/:80'); // --Heroku
 
 // PRIVATE EVENTS
 // Connection event
@@ -32,6 +34,7 @@ socket.onmessage = function (e) {
     switch (actioncode) {
         case PLAINTEXT:
           console.log('Server: ', primarydata);
+          alert(primarydata, "Incorrect Login Details");
           break;
         case SERVER_TO_CLIENT_COORDS:
           splitmessage.shift();
@@ -39,7 +42,29 @@ socket.onmessage = function (e) {
           break;
         case LOGIN_SUCCESS:
           login_response(primarydata);
+          break;
+        case DEATH:
+          death(primarydata);
+          break;
+        case LEADERBOARD:
+          leaderboard(primarydata.split('@'));
+          break;
     }
+};
+
+function leaderboard(data) {
+  for (var i in data) {
+    var username = data[i].split('#')[0];
+    var score = data[i].split('#')[1];
+    console.log(username, score);
+  }
+  table = document.getElementById('leaderboard');
+  table.rows[2].cells[1].innerHTML = data[0].split('#')[0];
+  table.rows[3].cells[1].innerHTML = data[1].split('#')[0];
+  table.rows[4].cells[1].innerHTML = data[2].split('#')[0];
+  table.rows[2].cells[2].innerHTML = data[0].split('#')[1];
+  table.rows[3].cells[2].innerHTML = data[1].split('#')[1];
+  table.rows[4].cells[2].innerHTML = data[2].split('#')[1];
 };
 
 // PRIVATE METHODS
@@ -47,6 +72,9 @@ socket.onmessage = function (e) {
 function internalsend(message) {
     // console.log('Sending: ', message);
     socket.send(message);
+    if (message.split(';')[0] == LOGIN) {
+      my_name = message.split(';')[1];
+    }
 };
 
 // PUBLIC METHODS
@@ -70,39 +98,105 @@ function sendcoords(x, y) {
     internalsend(CLIENT_TO_SERVER_COORDS + ';' + x + ';' + y);
 };
 
+
+(function (){
+    var canvas = document.getElementById("gamecanvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}());
+
+// PAGE CODE
+var score = 0;
+function increment(){
+  score += 1;
+}
+
 // PUBLIC EVENTS
 // Receive Co-ordinates
 function oncoords(data) {
-
+  // For each incoming player co-ord
   outer: for (var i in data) {
     var split_data = data[i].split('@');
-    console.log(split_data);
+    // console.log(split_data);
+    // For each existing player
     for (var s in snakes) {
       if (snakes[s].name === split_data[0]) {
         snakes[s].x.push(split_data[1]);
         snakes[s].y.push(split_data[2]);
+        snakes[s].t.push(Date.now());
         // console.log(snakes[i].x);
-        if (snakes[i].x.length > 200) {
-          snakes[i].x.shift();
-          snakes[i].y.shift();
+        /*
+        if (snakes[s].x.length > 5) {
+          snakes[s].x.shift();
+          snakes[s].y.shift();
+        }
+        */
+        var time = Date.now();
+        while (true) {
+          if (time - snakes[s].t[0] > 2000) {
+            snakes[s].t.shift();
+            snakes[s].x.shift();
+            snakes[s].y.shift();
+          } else {
+            break;
+          }
         }
         continue outer;
       }
     }
-    console.log('new');
+    //console.log('Adding: ' + split_data[0]);
     snakes.push(new Snake(split_data[0], split_data[1], split_data[2]));
+  }
+  outer: for (var i in snakes) {
+    for (var j in data) {
+      if (snakes[i].name === data[j].split('@')[0]) {
+        continue outer;
+      }
+    }
+    if (snakes[i].name === my_name) {
+      death(snakes[i].name);
+    } else {
+      death(snakes[i].name);
+    }
   }
   draw();
   tick();
-
 };
 
 // Login response
+var my_name;
 function login_response(message) {
     console.log(message);
-    player = new Player('ME', 250, 250);
+
+    var r = document.getElementById("UserLogin").style;
+    r.opacity = 1;
+
+    var table = document.getElementById("Leaderboard").style.visibility = "visible";
+    var scoreTable = document.getElementById("score").style.visibility = "visible";
+
+    (function fade() {
+        (r.opacity -= .1) < 0 ? r.display = "none" : setTimeout(fade, 40)
+    })();
+
+    (function fade() {
+        (scoreTable.opacity -= .1) < 0 ? scoreTable.display = "none" : setTimeout(fade, 40)
+    })();
+
     tick();
 };
+
+// Death response
+function death(player_name) {
+  //console.log('Kill: ' + player_name);
+  for (var i in snakes) {
+    if (snakes[i].name === my_name) {
+      score = 0;
+    }
+    if (snakes[i].name === player_name) {
+      snakes.splice(i,1);
+    }
+  }
+}
 
 class Player {
   constructor(in_name, in_x, in_y) {
@@ -117,12 +211,14 @@ class Snake {
     this.name = in_name;
     this.x = [in_x];
     this.y = [in_y];
+    this.t = [Date.now()];
   };
 };
 
 var snakes = [];
 var players = [];
 var player;
+var currentDirection;
 // PAGE CODE
 
 function tick() {
@@ -130,39 +226,53 @@ function tick() {
   var y_out = 0;
   switch (key) {
     case 'w':
-      y_out--;
-      break;
+      if(currentDirection !== "down"){
+        currentDirection = "up";
+        y_out--;
+        break;
+      } else {
+        y_out++;
+        break;
+      }
     case 'a':
-      x_out--;
-      break;
+      if(currentDirection !== "right"){
+        currentDirection = "left";
+        x_out--;
+        break;
+      } else {
+        x_out++;
+        break;
+      }
     case 's':
-      y_out++;
-      break;
+      if(currentDirection !== "up"){
+        currentDirection = "down";
+        y_out++;
+        break;
+      } else {
+        y_out--;
+        break;
+      }
     case 'd':
-      x_out++;
-      break;
+      if(currentDirection !== "left"){
+        currentDirection = "right";
+        x_out++;
+        break;
+      } else {
+        x_out--;
+        break;
+      }
   }
   // Constrain
   sendcoords(x_out , y_out);
 };
 
-// Set the enter key to activate send button
-/*
-var input = document.getElementById("sendbox");
-input.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-        document.getElementById("sendbutton").click();
-        input.value = '';
-    }
-});
-*/
+var interval = setInterval(increment,1000);
+
 // Only for debugging. Do not use internalsend
-/*
 function sendField() {
     var message = document.getElementById("sendbox").value;
     internalsend(message);
 }
-*/
 
 var frames = 0;
 // Sizing
@@ -170,46 +280,111 @@ var frames = 0;
 var w = window.innerWidth;
 var h = window.innerHeight;
 var max = w>h ? h : w;
+var scale = max / 500;
+
+//detect mobile deveice to set correct canvas size
+function detectmob() {
+   if(w <= 800 && h <= 600) {
+     return true;
+   } else {
+     return false;
+   }
+}
+
 // Set canvas size
 document.getElementById("gamecanvas").width = max;
 document.getElementById("gamecanvas").height = max;
 // set bounds
 document.getElementById("gamecanvas").style.position = "absolute";
 document.getElementById("gamecanvas").style.left = ((w - max)/2) + 'px';
+
 document.getElementById("gamecanvas").style.top = ((h - max)/2) + 'px';
+//if a mobile device is detected, move the canvas up so that there is room for buttons
+//document.getElementById("gamecanvas").style.top = (((h - max) / 2)-50) + 'px';
+
 // Draw
 function draw() {
   frames++;
+  document.getElementById("score").innerHTML = 'Score: ' + (score);
   var canvas = document.getElementById('gamecanvas');
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, max, max);
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, max, max);
-    ctx.fillStyle = 'white';
-    ctx.strokeRect(0, 0, max, max);
-    ctx.fillStyle = 'white';
+    //ctx.fillStyle = 'white';
     for (var i in snakes) {
-      ctx.lineWidth = 5;
+      // Glow
+      ctx.globalAlpha = 0.1;
+      ctx.strokeStyle = snakes[i].name == my_name ? '#16f34e' : '#f3bc16';
+      ctx.lineWidth = 15;
       ctx.beginPath();
-      var last_x = snakes[i].x[0];
-      var last_y = snakes[i].y[0];
+      var last_x = snakes[i].x[0] * scale;
+      var last_y = snakes[i].y[0] * scale;
       ctx.moveTo(last_x, last_y);
       for (var j in snakes[i].x) {
-        var x_dif = last_x - snakes[i].x[j];
-        var y_dif = last_y - snakes[i].y[j];
-        if ( !(x_dif > 10 || x_dif < -10 || y_dif > 10 || y_dif < -10) ) {
-          ctx.lineTo(snakes[i].x[j], snakes[i].y[j]);
+        var x_dif = last_x - snakes[i].x[j] * scale;
+        var y_dif = last_y - snakes[i].y[j] * scale;
+        if ( !(x_dif > 50 * scale || x_dif < -50 * scale || y_dif > 50 * scale || y_dif < -50 * scale) ) {
+          ctx.lineTo(snakes[i].x[j] * scale, snakes[i].y[j] * scale);
         } else {
-          ctx.strokeStyle = "white";
           ctx.stroke();
           ctx.beginPath();
-          ctx.moveTo(snakes[i].x[j], snakes[i].y[j]);
+          ctx.moveTo(snakes[i].x[j] * scale, snakes[i].y[j] * scale);
         }
-        last_x = snakes[i].x[j];
-        last_y = snakes[i].y[j];
+        last_x = snakes[i].x[j] * scale;
+        last_y = snakes[i].y[j] * scale;
       }
-      ctx.strokeStyle = "white";
       ctx.stroke();
+      // Background
+      ctx.globalAlpha = 1;
+      ctx.strokeStyle = snakes[i].name == my_name ? '#16f34e' : '#f3bc16';
+      ctx.lineWidth = 7;
+      ctx.beginPath();
+      var last_x = snakes[i].x[0] * scale;
+      var last_y = snakes[i].y[0] * scale;
+      ctx.moveTo(last_x, last_y);
+      for (var j in snakes[i].x) {
+        var x_dif = last_x - snakes[i].x[j] * scale;
+        var y_dif = last_y - snakes[i].y[j] * scale;
+        if ( !(x_dif > 50 * scale || x_dif < -50 * scale || y_dif > 50 * scale || y_dif < -50 * scale) ) {
+          ctx.lineTo(snakes[i].x[j] * scale, snakes[i].y[j] * scale);
+        } else {
+          ctx.stroke();
+          ctx.beginPath();
+          ctx.moveTo(snakes[i].x[j] * scale, snakes[i].y[j] * scale);
+        }
+        last_x = snakes[i].x[j] * scale;
+        last_y = snakes[i].y[j] * scale;
+      }
+      ctx.stroke();
+      // Foreground
+      ctx.globalAlpha = 0.5;
+      ctx.strokeStyle = 'white';
+      ctx.lineWidth = 3;
+      ctx.beginPath();
+      var last_x = snakes[i].x[0] * scale;
+      var last_y = snakes[i].y[0] * scale;
+      ctx.moveTo(last_x, last_y);
+      for (var j in snakes[i].x) {
+        var x_dif = last_x - snakes[i].x[j] * scale;
+        var y_dif = last_y - snakes[i].y[j] * scale;
+        if ( !(x_dif > 50 * scale || x_dif < -50 * scale || y_dif > 50 * scale || y_dif < -50 * scale) ) {
+          ctx.lineTo(snakes[i].x[j] * scale, snakes[i].y[j] * scale);
+        } else {
+          ctx.stroke();
+          ctx.beginPath();
+          ctx.moveTo(snakes[i].x[j] * scale, snakes[i].y[j] * scale);
+        }
+        last_x = snakes[i].x[j] * scale;
+        last_y = snakes[i].y[j] * scale;
+      }
+      ctx.stroke();
+      // Draw name
+      ctx.globalAlpha = 1;
+      ctx.fillStyle = 'white';
+      ctx.font = "20px Comic Sans MS";
+      ctx.fillText(snakes[i].name, (parseInt(snakes[i].x[snakes[i].x.length-1]) + 5) * scale, (parseInt(snakes[i].y[snakes[i].y.length-1]) - 5) * scale);
     }
   }
 };
@@ -220,7 +395,6 @@ setInterval(function fps() {
 }, 1000);
 
 draw();
-
 var key = 'w';
 
 document.onkeydown = function (e) {
@@ -240,56 +414,81 @@ document.onkeydown = function (e) {
   }
 };
 
-var score = 1;
-
-var interval = setInterval(increment,1000);
-
-function increment() {
-  i += 1;
+function mobileBtnLeft(){
+    key = 'a';
 }
 
+function mobileBtnUp(){
+    key = 'w';
+}
 
-// PAGE CODE
+function mobileBtnDown(){
+    key = 's';
+}
 
-// Set the enter key to activate send button
-// var input = document.getElementById("sendmessage");
-// input.addEventListener("keyup", function (event) {
-//     if (event.keyCode === 13) {
-//         document.getElementById("sendbutton").click();
-//         input.value = '';
-//     }
-// });
-
-// Only for debugging. Do not use internalsend
-// function sendField() {
-//     var message = document.getElementById("sendmessage").value;
-//     internalsend(message);
-// }
+function mobileBtnRight(){
+    key = 'd';
+}
 
 function submitBtnPress() {
+
+    score = 0;
 
     var usrName = document.getElementById("UsernameInput").value;
     var usrPsswd = document.getElementById("UsernamePassword").value;
 
     if (usrName != "" && usrPsswd != "") {
         login(usrName, usrPsswd);
-
-        var r = document.getElementById("UserLogin").style;
-        r.opacity = 1;
-
-        (function fade() {
-            (r.opacity -= .1) < 0 ? r.display = "none" : setTimeout(fade, 40)
-        })();
-
     } else{
         alert("You must enter username and password");
+    }
+}
+
+//function to validate users password input
+function checkPassword(userInput) {
+    var validation = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+    return validation.test(userInput);
+  }
+
+function createBtnPress() {
+
+    var createUsrName = document.getElementById("createUsernameInput").value;
+    var createUsrPsswd = document.getElementById("createUsernamePassword").value;
+    var confirmUsrPsswd = document.getElementById("confirmUsernamePassword").value;
+
+    validationCheck = /^\w+$/;
+
+    if(createUsrName != ""){
+        if(!validationCheck.test(createUsrName)){
+            alert("Username must contain only letters, numbers and underscores");
+            createUsrName.focus();
+        }
+    }else{
+       alert("Username cannot be left blank");
+    }
+
+    if(createUsrPsswd != confirmUsrPsswd || confirmUsrPsswd == ""){
+        alert("Re-entered password MUST be the same as password");
+    }
+
+    if (createUsrPsswd != "" && confirmUsrPsswd == createUsrPsswd) {
+        if(false){// De-activated
+            alert("Password MUST contain at least one number/lowercase/uppercase letter and be at least 6 characaters in length");
+            createUsrPsswd.focus();
+        }
+        else{
+            register(createUsrName, createUsrPsswd);
+            //alert("Username and Password are VALID");
+        }
+    }else{
+        alert("Password cannot be left blank");
+        createUsrPsswd.focus();
     }
 }
 
 function btnPress() {
     var s = document.getElementById("PlayMenu").style;
     var r = document.getElementById("UserLogin").style.visibility = "visible";
-
 
     s.opacity = 1;
     (function fade() {
@@ -301,16 +500,10 @@ function registerBtnPress() {
 
     var s = document.getElementById("UserLogin").style.visibility = "hidden";
     var r = document.getElementById("RegisterUser").style.visibility = "visible";
-
-  }
+}
 
 function loginBtnPress() {
 
-    var s = document.getElementById("RegisterUser").style = "hidden";
+    var s = document.getElementById("RegisterUser").style.visibility = "hidden";
     var r = document.getElementById("UserLogin").style.visibility = "visible";
-
-  }
-
-function fade(element) {
-    (element.opacity += .1) < 0 ? element.display = "none" : setTimeout(fade, 70)
-        };
+}
